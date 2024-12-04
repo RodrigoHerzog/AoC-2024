5ef33718d9be53feecc9a51fff669b6cd5c5671c
RodrigoHerzog <rodrigo.h.g.dev@gmail.com>
2024-12-04 01:15:46 -0300

    Refactored Day3 to use regexp

--- Day3-Mull_It_Over/day3.go
+++ Day3-Mull_It_Over/day3.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
 )
 
@@ -15,100 +16,35 @@ func main() {
 }
 
 func GetUncorruptedMul(memory_list []string, conditional_enabled bool) int {
+	regexp_instructions := `mul\((\d{1,3}),(\d{1,3})\)`
+	if conditional_enabled {
+		regexp_instructions += `|do\(\)|don't\(\)`
+	}
+
 	sum_of_mul := 0
 	mul_instruction_enabled := true
+
 	for _, memory_block := range memory_list {
-		for idx := 0; idx < len(memory_block); idx++ {
-			first_mul_num := ""
-			second_mul_num := ""
-			mul_is_valid := false
+		re := regexp.MustCompile(regexp_instructions)
+		memory_instructions := re.FindAllStringSubmatch(memory_block, -1)
 
-			if conditional_enabled {
-				mul_instruction_enabled = FindConditional(idx, memory_block, mul_instruction_enabled)
-			}
-
-			if mul_instruction_enabled {
-				first_mul_num, second_mul_num, mul_is_valid = FindMultiplication(idx, memory_block, first_mul_num, second_mul_num, mul_is_valid)
-			}
-
-			if mul_is_valid {
-				first_mul_num, err := strconv.Atoi(first_mul_num)
-				if err != nil {
-					log.Fatal(err)
+		for _, instruction := range memory_instructions {
+			switch instruction[0] {
+			case "do()":
+				mul_instruction_enabled = true
+			case "don't()":
+				mul_instruction_enabled = false
+			default:
+				if mul_instruction_enabled {
+					first_mul_num, _ := strconv.Atoi(instruction[1])
+					second_mul_num, _ := strconv.Atoi(instruction[2])
+					sum_of_mul += first_mul_num * second_mul_num
 				}
-				second_mul_num, err := strconv.Atoi(second_mul_num)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				sum_of_mul += first_mul_num * second_mul_num
 			}
 		}
 	}
+
 	return sum_of_mul
-}
-
-func FindConditional(idx int, memory_block string, mul_instruction_enabled bool) bool {
-	if memory_block[idx] == 'd' {
-		if memory_block[idx+1] == 'o' {
-			switch memory_block[idx+2] {
-			case '(':
-				if memory_block[idx+3] == ')' {
-					mul_instruction_enabled = true
-				}
-			case 'n':
-				if memory_block[idx+3] == '\'' {
-					if memory_block[idx+4] == 't' {
-						if memory_block[idx+5] == '(' {
-							if memory_block[idx+6] == ')' {
-								mul_instruction_enabled = false
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	return mul_instruction_enabled
-}
-
-func FindMultiplication(idx int, memory_block string, first_mul_num string, second_mul_num string, mul_is_valid bool) (string, string, bool) {
-	if memory_block[idx] == 'm' {
-		if memory_block[idx+1] == 'u' {
-			if memory_block[idx+2] == 'l' {
-				if memory_block[idx+3] == '(' {
-					for nidx := 1; nidx < 4; nidx++ {
-						if memory_block[idx+3+nidx] >= '0' &&
-							memory_block[idx+3+nidx] <= '9' {
-							first_mul_num += string(memory_block[idx+3+nidx])
-						}
-						if memory_block[idx+3+nidx] == ',' &&
-							nidx != 1 {
-							break
-						}
-					}
-					if first_mul_num != "" &&
-						memory_block[idx+4+len(first_mul_num)] == ',' {
-						for nidx := 1; nidx < 4; nidx++ {
-							if memory_block[idx+4+len(first_mul_num)+nidx] >= '0' &&
-								memory_block[idx+4+len(first_mul_num)+nidx] <= '9' {
-								second_mul_num += string(memory_block[idx+4+len(first_mul_num)+nidx])
-							}
-							if memory_block[idx+4+len(first_mul_num)+nidx] == ')' &&
-								nidx != 1 {
-								break
-							}
-							if second_mul_num != "" &&
-								memory_block[idx+5+len(first_mul_num)+len(second_mul_num)] == ')' {
-								mul_is_valid = true
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	return first_mul_num, second_mul_num, mul_is_valid
 }
 
 func ReadInput(path string) []string {
@@ -117,15 +53,14 @@ func ReadInput(path string) []string {
 		log.Fatal(err)
 	}
 
+	defer file.Close()
+
 	scanner := bufio.NewScanner(file)
 	input_list := []string{}
 	for scanner.Scan() {
 		line := scanner.Text()
 		input_list = append(input_list, line)
-
 	}
-
-	defer file.Close()
 
 	return input_list
 }
